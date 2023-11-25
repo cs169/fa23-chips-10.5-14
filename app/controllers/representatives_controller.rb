a2be7fb3d16b5216bc50de9ff44c811e7d65a415
bentref <bentref11@gmx.com>
2023-11-25 10:27:50 -0800

    Linter fixes

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,6 +4,7 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
+
   def show
     id = params[:id]
     @representative = Representative.find(id)
