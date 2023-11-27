effe10645ec5cab4656695917d3265f256be3dd4
Lucas Lovric <lucaslovric@berkeley.edu>
2023-11-27 02:30:20 +0000

    done

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -17,21 +17,11 @@ class Representative < ApplicationRecord
         end
       end
 
-      # rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-      #    title: title_temp })
-      # reps.push(rep)
-
-      # Check if a representative with the same name already exists
-      existing_representative = Representative.find_by(name: official.name)
-      if existing_representative
-        # Handle the case where a representative with the same name exists
-        # You might want to update attributes or log a message here
-        puts "Representative with name '#{official.name}' already exists."
-      else
-        # Create a new representative if it doesn't already exist
-        rep = Representative.create!({ name: official.name, ocdid: ocdid_temp, title: title_temp })
-        reps.push(rep)
-      end
+      rep = Representative.find_or_initialize_by(name: official.name)
+      rep.title = title_temp
+      rep.ocdid = ocdid_temp
+      rep.save!
+      reps.push(rep)
     end
 
     reps
--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -2,33 +2,36 @@
 
 require 'rails_helper'
 
-RSpec.describe Representative, type: :model do
+require 'google/apis/civicinfo_v2'
+
+describe Representative do
   describe '.civic_api_to_representative_params' do
-    it 'raises an error when trying to insert a duplicate representative' do
-      # Set up a representative with the same name, ocdid, and title in the database
-      existing_representative = described_class.create!(
-        name: 'John Doe', 
-        ocdid: 123, 
-        title: 'Existing Title')
-
-      # Mock API data for testing
-      rep_info = double('rep_info', 
-        officials: [double(
-          'official',
-          name: 'John Doe')
-        ],
-        offices: [double(
-          'office',
-          name: 'Office',
-          division_id: 123,
-          official_indices: [0])
-        ]
+    let(:offices) do
+      instance_double(
+        Google::Apis::CivicinfoV2::Office,
+        name: 'Office1',
+        division_id: 'Division1', official_indices: [0]
       )
+    end
+    let(:officials) do
+      instance_double(Google::Apis::CivicinfoV2::Official,
+                      name: 'Gavin Newsom')
+    end
 
-      # Expect an error when trying to create a duplicate representative
-      expect do
-        described_class.civic_api_to_representative_params(rep_info)
-      end.not_to raise_error
+    let(:rep_info) do
+      instance_double(
+        Google::Apis::CivicinfoV2::RepresentativeInfoResponse,
+        officials: [officials],
+        offices:   [offices]
+      )
+    end
+
+    it 'Representative already exists' do
+      expect(described_class.count).to eq(0)
+      # Insert the same representative twice
+      described_class.civic_api_to_representative_params(rep_info)
+      reps = described_class.civic_api_to_representative_params(rep_info)
+      expect(reps.count).to eq(described_class.count)
     end
   end
 end
